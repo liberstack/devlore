d1946b892ab1306f315f9563cb17e1b54adb1119
ribeirorep <topverbs@gmail.com>
2026-07-18 22:49:49 -0300

    quad

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ export const CONFIG = {
 
   footer: {
     about:
-      "Este projeto é totalmente estático e roda no navegador: não há backend, banco de dados ou coleta de dados do usuário. Nenhuma informação pessoal é armazenada, rastreada ou compartilhada com terceiros. O conteúdo é fornecido apenas para fins informativos. Este é um projeto de arquivo digital estático, construído apenas com HTML, CSS e JavaScript puro. Nenhuma dependência além do marked.js para renderizar Markdown.",
+      "Meu nome é Guilherme Ribeiro. Este projeto é totalmente estático e roda no navegador: não há backend, banco de dados ou coleta de dados do usuário. Nenhuma informação pessoal é armazenada, rastreada ou compartilhada com terceiros. O conteúdo é fornecido apenas para fins informativos. Este é um projeto de arquivo digital estático, construído apenas com HTML, CSS e JavaScript puro. Nenhuma dependência além do marked.js para renderizar Markdown.",
     contact: {
       email: "topverbs@gmail.com",
       whatsapp: "liberstack",
